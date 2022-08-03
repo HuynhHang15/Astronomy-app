ab409016e69d791fbb804858e552cb466dbda60b
Huynh Hang <huynhhang1505@gmail.com>
2022-08-03 12:00:28 +0700

    add shrink header

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -3,6 +3,7 @@ import style from "./Header.module.scss";
 
 import logoImg from "~/asset/images/NASA_logo.png";
 import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 
 const cx = classNames.bind(style);
 
@@ -22,8 +23,27 @@ const headerNav = [
 ];
 
 function Header() {
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const shrinkHeader = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        headerRef.current.classList.add("shrink");
+      } else {
+        headerRef.current.classList.remove("shrink");
+      }
+    };
+    window.addEventListener("scroll", shrinkHeader);
+    return () => {
+      window.removeEventListener("scroll", shrinkHeader);
+    };
+  }, []);
+
   return (
-    <div className={cx("header")}>
+    <div ref={headerRef} className={cx("header")}>
       <div className={cx("container")}>
         <div className={cx("header-logo")}>
           <img src={logoImg} alt="logo" />
